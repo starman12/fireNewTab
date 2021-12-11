13ea207393e3a08dd78d89485cd35c627dd8bf5b
starman12 <45459582+starman12@users.noreply.github.com>
2021-12-11 16:23:09 +0800

    Add files via upload

--- engine.js
+++ engine.js
@@ -4,23 +4,30 @@ let date = new Date()
 let bar = document.getElementById('liquid')
 // let writing = document.getElementById('letter')
 
+// if (localStorage.getItem('nighted') == null) {
+//     localStorage.setItem('nighted', true)
+// }
+
+// if (localStorage.getItem('nighted') == true || !slocalStorage.getItem('startDate')) {
+//     localStorage.setItem('startDate', date)
+//     localStorage.setItem('nighted', false)
+// }
+
 let countdown = setInterval(() => {
-    
     date = new Date()
     time.innerHTML = `${(19 - date.getHours()).toString().padStart(2, '0')} ${(59 - date.getMinutes()).toString().padStart(2, '0')}<span class='s'> ${(59 - date.getSeconds()).toString().padStart(2, '0')}</span>`
-    //if (date.getHours() > 20) {
-    //    localStorage.setItem('nighted', true)
-    //    clearInterval(countdown)
-    //    time.innerHTML = `<span class=s>Snoozers, it's</span> \n Bedtime`
-    //}
-    //else {
-    //    updateBar(date)
-    //}
+    // if (date.getHours() > 20) {
+    //     localStorage.setItem('nighted', true)
+    //     clearInterval(countdown)
+    //     time.innerHTML = `<span class=s>Snoozers, it's</span> \n Bedtime`
+    // }
+    // else {
+    //     updateBar(date)
+    // }
 }, 1000);
 
 function updateBar(date) {
-    let sd = localStorage.getItem('startDate')
-    let dayhours = 19 - sd.getHours()
+    let dayhours = 19 - localStorage.getItem('startDate')?.getHours()
     bar.style.width = (19 - date.getHours()) / dayhours + '%'
 }
 
