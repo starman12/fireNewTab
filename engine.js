82b195a545a2058ea13e4d0af5e1b46f80e1bc23
starman12 <45459582+starman12@users.noreply.github.com>
2021-12-11 16:13:35 +0800

    Update engine.js

--- engine.js
+++ engine.js
@@ -13,28 +13,20 @@ if (localStorage.getItem('nighted') == true || !localStorage.getItem('startDate'
     localStorage.setItem('nighted', false)
 }
 
-console.log(localStorage.getItem('startDate'))
+console.log(localStorage.getItem('startDate').getHour())
 
 let countdown = setInterval(() => {
     
-if (localStorage.getItem('nighted') == null) {
-    localStorage.setItem('nighted', true)
-}
-
-if (localStorage.getItem('nighted') == true || !localStorage.getItem('startDate')) {
-    localStorage.setItem('startDate', new Date())
-    localStorage.setItem('nighted', false)
-}
     date = new Date()
     time.innerHTML = `${(19 - date.getHours()).toString().padStart(2, '0')} ${(59 - date.getMinutes()).toString().padStart(2, '0')}<span class='s'> ${(59 - date.getSeconds()).toString().padStart(2, '0')}</span>`
-    if (date.getHours() > 20) {
-        localStorage.setItem('nighted', true)
-        clearInterval(countdown)
-        time.innerHTML = `<span class=s>Snoozers, it's</span> \n Bedtime`
-    }
-    else {
-        updateBar(date)
-    }
+    //if (date.getHours() > 20) {
+    //    localStorage.setItem('nighted', true)
+    //    clearInterval(countdown)
+    //    time.innerHTML = `<span class=s>Snoozers, it's</span> \n Bedtime`
+    //}
+    //else {
+    //    updateBar(date)
+    //}
 }, 1000);
 
 function updateBar(date) {
